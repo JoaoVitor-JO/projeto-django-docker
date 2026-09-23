2e160316928ba7a974f72ec625eadf8a69cf6a63
joaoVitor-JO <joao-ubunto@DESKTOP-TR6SR9V.localdomain>
2026-09-23 14:55:21 -0300

    feature(settings): Realiza configuração do settings baseado na env e configura url para debug

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -11,21 +11,24 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR.parent / 'data' / 'web'
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+w4$6(-=uaf8$z6pav8m^wreid9533*c0hz9yos%s_hn-9v=n@'
-
+SECRET_KEY = os.getenv('SECRET_KEY', 'change-me')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.getenv('DEBUG',0)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    h.strip() for h in os.getenv('ALLOWED_HOSTS','').split(',')
+    if h.strip()
+]
 
 
 # Application definition
@@ -74,8 +77,12 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : os.getenv('DB_ENGINE','change-me'),
+        'NAME' : os.getenv('POSTGRES_DB','change-me'),
+        'USER' : os.getenv('POSTGRES_USER','change-me'),
+        'PASSWORD' : os.getenv('POSTGRES_PASSWORD','change-me'),
+        'HOST' : os.getenv('POSTGRES_HOST','change-me'),
+        'PORT' : os.getenv('POSTGRES_PORT','change-me'),
     }
 }
 
@@ -102,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -114,7 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
+#caminho dos arquivos staticos do projeto
 STATIC_URL = 'static/'
+STATIC_ROOT = DATA_DIR /'static'
+
+#caminho dos arquivos de  media do projeto
+MEDIA_URL = DATA_DIR /'media'
+MEDIA_ROOT = DATA_DIR / 'media'
 
 
 # Email
